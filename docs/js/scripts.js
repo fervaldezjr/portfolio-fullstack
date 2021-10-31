07216647cd4226119a4a51f4ccff9c43539607c6
Fer Valdez <fervaldezjr11@gmail.com>
2021-10-31 22:43:46 +0100

    add all files

---
+++ docs/js/scripts.js
@@ -0,0 +1,10 @@
+ScrollReveal().reveal('.primero', { delay: 600 });
+ScrollReveal().reveal('.segundo', { delay: 700 });
+ScrollReveal().reveal('.tercero', { delay: 800 });
+ScrollReveal().reveal('.cuarto', { delay: 900 });
+ScrollReveal().reveal('.quinto', { delay: 1000 });
+ScrollReveal().reveal('.sexto', { delay: 1100 });
+ScrollReveal().reveal('.septimo', { delay: 1200 });
+ScrollReveal().reveal('.octavo', { delay: 1500 });
+ScrollReveal().reveal('.noveno', { delay: 1600 });
+
