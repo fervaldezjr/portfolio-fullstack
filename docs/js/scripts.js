a605ab9cbfcb9af93a82d78915b26d56333d7b63
Fer Valdez <fervaldezjr11@gmail.com>
2021-11-05 01:39:41 +0100

    mejoras de muchos archivos, images, etc. confeccion de seccion contacto

--- docs/js/scripts.js
+++ docs/js/scripts.js
@@ -1,10 +1,10 @@
-ScrollReveal().reveal('.primero', { delay: 600 });
-ScrollReveal().reveal('.segundo', { delay: 700 });
-ScrollReveal().reveal('.tercero', { delay: 800 });
-ScrollReveal().reveal('.cuarto', { delay: 900 });
-ScrollReveal().reveal('.quinto', { delay: 1000 });
-ScrollReveal().reveal('.sexto', { delay: 1100 });
-ScrollReveal().reveal('.septimo', { delay: 1200 });
-ScrollReveal().reveal('.octavo', { delay: 1500 });
-ScrollReveal().reveal('.noveno', { delay: 1600 });
+ScrollReveal().reveal('.primero', { delay: 400 });
+ScrollReveal().reveal('.segundo', { delay: 500 });
+ScrollReveal().reveal('.tercero', { delay: 600 });
+ScrollReveal().reveal('.cuarto', { delay: 700 });
+ScrollReveal().reveal('.quinto', { delay: 800 });
+ScrollReveal().reveal('.sexto', { delay: 900 });
+ScrollReveal().reveal('.septimo', { delay: 1000 });
+ScrollReveal().reveal('.octavo', { delay: 1100 });
+ScrollReveal().reveal('.noveno', { delay: 1200 });
 
